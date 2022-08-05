bdc56097dc85f3348522d1679a7924afe181b52a
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2022-08-04 22:11:47 -0300

    update. cors

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:8000';
+  httpHeaders = new HttpHeaders({ 'Content-Type': 'application.json'});
+
+  constructor(private http: HttpClient) { }
+
+  getAllMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/movies/`,
+      { headers: this.httpHeaders });
+  }
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  movies = [{title:'filme1', desc:'desc1'},{title:'filme2', desc:'desc2'}];
+
+  movies = [{
+    title: 'title',
+    desc: 'desc'
+  }];
+
+  constructor(private api: ApiService) {
+    this.getMovies();
+  }
+
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error);
+    });
+  }
+
 }
